776414c6099385f349eea400d8621baeec49dbd6
Vauff <mctehkitti@gmail.com>
2016-09-23 20:06:12 -0400

    Change domain name

--- RenewSSLCert.sh
+++ RenewSSLCert.sh
@@ -1,7 +1,7 @@
 echo Creating new certificate...
 openssl genrsa 4096 > account.key
 openssl genrsa 4096 > domain.key
-openssl req -new -sha256 -key domain.key -subj "/CN=geforcemods.net" > domain.csr
+openssl req -new -sha256 -key domain.key -subj "/CN=vauff.me" > domain.csr
 sudo python acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir /var/www/html/.well-known/acme-challenge/ > ./signed.crt
 cat domain.key signed.crt lets-encrypt-x1-cross-signed.pem > certificate.pem
 cp certificate.pem /home/Vauff/.znc/znc.pem
