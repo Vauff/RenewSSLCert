bbeaf77f1f1ed67e73f026502b32f5c94b5ba841
Vauff <mctehkitti@gmail.com>
2016-09-25 17:10:10 -0400

    Update Let's Encrypt .pem

--- RenewSSLCert.sh
+++ RenewSSLCert.sh
@@ -3,7 +3,7 @@ openssl genrsa 4096 > account.key
 openssl genrsa 4096 > domain.key
 openssl req -new -sha256 -key domain.key -subj "/CN=vauff.me" > domain.csr
 python acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir /var/www/html/.well-known/acme-challenge/ > ./signed.crt
-cat domain.key signed.crt lets-encrypt-x1-cross-signed.pem > certificate.pem
+cat domain.key signed.crt lets-encrypt-x3-cross-signed.pem > certificate.pem
 cp certificate.pem /home/Vauff/.znc/znc.pem
 cp certificate.pem /home/Vauff/.config/quassel-irc.org/quasselCert.pem
 cp domain.key /etc/httpd
